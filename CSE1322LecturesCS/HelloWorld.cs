e74ab6ce86c2ab4c728f635fbfe05fefb5b46ca8
CSITEd Experts <CSITEdExperts@gmail.com>
2018-09-21 09:31:07 -0400

    Initial codes added

---
+++ CSE1322LecturesCS/HelloWorld.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSE1322LecturesCS
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Lecture11Ex4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture11Ex4
+{
+    class Program
+    {
+        class Z { public int y; }
+        static void Main(String[] args)
+        {
+            Z myZ = new Z();
+            myZ.y = 42;
+            Console.WriteLine(myZ.y);
+            B(myZ);
+            Console.WriteLine(myZ.y);
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+        static void B(Z x)
+        {
+            x.y += 9;
+            Console.WriteLine(x.y);
+        }
+    }
+}
+
