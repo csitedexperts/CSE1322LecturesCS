f05a65208a54191559790265d5b05d22e3a39f5c
CSITEd Experts <csitedexperts@gmail.com>
2019-10-24 18:58:59 -0400

    DoublyLinkedList implementation added

---
+++ DoublyLinkedListImplemented/Node.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoublyLinkedListImplemented
+{
+    
+        public class Node
+        {
+            public int id;
+            public  String name;
+            public Node next;
+            public Node prev;
+
+            public Node(int id, String name, Node next, Node prev)
+            {
+                this.id = id;
+                this.name = name;
+                this.next = next;
+                this.prev = prev;
+            }
+
+            public void displayANode()
+            {
+                Console.WriteLine("id: " + id + " name:" + name);
+            }
+
+         public override String ToString()
+            {   // Required
+                return "" + id + " " + name;
+            }
+
+        }
+    }
---
+++ DoublyLinkedListImplemented/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoublyLinkedListImplemented
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DoublyLinkedList dll = new DoublyLinkedList();
+            Console.WriteLine(dll.size());
+
+            dll.append(1, "A");
+            dll.append(2, "B");
+            dll.prepend(3, "C");
+            dll.append(4, "D");
+            dll.append(5, "E");
+
+            dll.displayAllNodes();
+
+            dll.iterateForward();
+            dll.removeFirst();
+            dll.removeLast();
+            dll.iterateBackward();
+            Console.ReadKey();
+
+        }
+    }
+
+}
